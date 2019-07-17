90eda3cbc2c5f5ba6c2524fad338f85bd03a01a3
Guilherme Calvi Ferreira <guilherme_ferreira@hst.com.br>
2019-07-17 17:25:44 -0300

    Teste1

--- src/main/java/mavenReleaseTest/MainClass.java
+++ src/main/java/mavenReleaseTest/MainClass.java
@@ -4,7 +4,7 @@ public class MainClass {
 	
 	public static void main(String[] args) {
 		System.out.println("Classe principal.");
-		System.out.println();
+		System.out.println("teste1");
 		System.out.println("A versão do seu projeto é v."+MainClass.class.getPackage().getImplementationVersion());
 	}
 
