186827b2f48302f9b43853d644224a33b38dc81a
Guilherme Calvi Ferreira <guilherme_ferreira@hst.com.br>
2019-07-17 14:35:27 -0300

    Commitando uno

--- src/main/java/mavenReleaseTest/MainClass.java
+++ src/main/java/mavenReleaseTest/MainClass.java
@@ -5,7 +5,7 @@ public class MainClass {
 	public static void main(String[] args) {
 		System.out.println("Classe principal.");
 		System.out.println();
-		System.out.println("A versão do seu projeto é v"+MainClass.class.getPackage().getImplementationVersion());
+		System.out.println("A versão do seu projeto é v."+MainClass.class.getPackage().getImplementationVersion());
 	}
 
 }
